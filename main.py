6c14a9b6979d40006b8f6c400eb1f4f2db4cdab2
Heng Ji Yong <hengjiyong@gmail.com>
2025-09-01 14:42:51 +0800

    Initial Commit

---
+++ main.py
@@ -0,0 +1,78 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from datetime import datetime
+import requests
+import plotly.express as px
+import plotly.graph_objects as go
+import logging
+
+DATABASE_ID = '1642c9a5264b800794b5e0fff150bbea'
+NOTION_TOKEN = 'secret_Vf2Yb1QHr1IOR8o2qs1OR88jDMP21ORSBJg4DHmOdvN'
+base_url = f'https://api.notion.com/v1/databases/{DATABASE_ID}/query'
+
+def get_current_month_pages():
+    # current_date = datetime.today()
+    current_date = datetime(2025, 8, 31)
+    current_date_iso = current_date.isoformat()
+    first_day_of_month = current_date.replace(day=1).isoformat()
+    filter = {
+        "filter" :
+            {
+                "and": 
+                [
+                    {
+                        "timestamp": "created_time",
+                        "created_time": {
+                            "after":first_day_of_month
+                        }
+                    },
+                    {
+                        "timestamp": "created_time",
+                        "created_time": {
+                            "before":current_date_iso
+                        }
+                    }
+                ]
+            }
+
+    }
+
+    headers = {'Authorization':'Bearer '+NOTION_TOKEN,
+              'Notion-Version': '2022-06-28',
+              'Content-Type':'application/json'}
+
+    res = requests.post(base_url, headers=headers, json=filter)
+    data = res.json()
+    result = data['results']
+    return result
+
+# def get_spending(results):
+#     spendings_categories = []
+#     for result in results:
+#         spending = result['properties']['Spending']['number']
+#         category = result['properties']['Tags']['select']['name']
+#         spendings_categories.append((category, spending))
+    
+#     df = pd.DataFrame(spendings_categories, columns=['Category', 'Spending'])
+#     grouped_data = df.groupby('Category')['Spending'].sum()
+#     return grouped_data
+
+def get_spending(results):
+    spendings_categories = []
+    spendings = []
+    categories = []
+    for result in results:
+        spending = result['properties']['Spending']['number']
+        category = result['properties']['Tags']['select']['name']
+        spendings.append(spending)
+        categories.append(category)
+    return categories, spendings
+
+if __name__ == '__main__':
+    current_month_spending = get_current_month_pages()
+    cat, spend = get_spending(current_month_spending)
+    
+    fig = go.Figure(data=[go.Pie(labels=cat , values=spend)])
+    st.plotly_chart(fig, use_container_width=False)
+    st.write('This one is the Heng')
